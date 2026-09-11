1bcaee966a337f13f4e4e327ee02d4bf008ec37d
Edson <edin2008@yahoo.com.br>
2026-09-11 12:05:47 -0300

    Navegação entre etapas

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react'
 import { simulationFormSteps } from '@/data/Simulation'
 import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
-  const currentStep = simulationFormSteps[5]
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+
+  //lógica
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  //logica
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev - 1)
+  }
 
   return (
     <>
-      <StepProgress currentStep={6} totalSteps={8} />
-      <FormStep key={currentStep.id} {...currentStep} />
+      <StepProgress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
+      <FormStep
+        key={currentStep.id}
+        {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        hideBackButton={currentStepIndex === 0}
+      />
     </>
   )
 }
