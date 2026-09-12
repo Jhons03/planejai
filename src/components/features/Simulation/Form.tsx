84793c3a87fd0fd5372fd97cf595208b563da0a8
Edson <edin2008@yahoo.com.br>
2026-09-12 20:38:11 -0300

    Criando Local storage

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
-import { simulationFormSteps } from '@/data/Simulation'
+import { useNavigate } from 'react-router-dom'
+import { simulationFormSteps, type SimulationFormData } from '@/data/Simulation'
+import { useSimulationStorage } from '@/hooks/useSimulationStorage'
 import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
+  const { saveFormData } = useSimulationStorage()
+  const navigate = useNavigate()
   const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const [formData, setFormData] = useState<SimulationFormData>({} as SimulationFormData)
   const totalSteps = simulationFormSteps.length
   const currentStep = simulationFormSteps[currentStepIndex]
 
   //lógica
-  const handleNextStep = () => {
+  const handleNextStep = (value: string) => {
+    const updatedFormData = { ...formData, [currentStep.id]: value }
+    setFormData(updatedFormData)
+
     if (currentStepIndex + 1 > totalSteps - 1) {
+      saveFormData(updatedFormData)
+      void navigate('/resultado')
       return
     }
     setCurrentStepIndex((prev) => prev + 1)
